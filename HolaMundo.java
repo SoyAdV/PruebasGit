c2e2de360472ac35ab2e77b11a1eeae2edb5913c
SoyAdV <79457088+SoyAdV@users.noreply.github.com>
2026-07-24 16:02:32 -0600

    Create HolaMundo.java
    Se sube clase

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    // Método principal: punto de entrada del programa
+    public static void main(String[] args) {
+    // Imprime el mensaje en la consola
+    System.out.println("¡Hola, Mundo!");
+    }
+}
