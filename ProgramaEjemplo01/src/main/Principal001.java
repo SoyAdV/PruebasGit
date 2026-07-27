a459de1a39f5654ce75cf31ddff8dc00762ba521
SoyAdV <79457088+SoyAdV@users.noreply.github.com>
2026-07-27 12:54:44 -0600

    Update Principal001.java
    Actulizacion de main

--- ProgramaEjemplo01/src/main/Principal001.java
+++ ProgramaEjemplo01/src/main/Principal001.java
@@ -4,11 +4,11 @@ public class Principal001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hola");
+		System.out.print("Hola1");
 		System.out.print("Hola2");
-		System.out.print("Hola");
 		System.out.print("Hola3");
 		System.out.print("Hola4");
+		System.out.print("Hola5");
 
 	}
 
