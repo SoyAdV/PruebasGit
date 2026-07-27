59ad77aa435cf2bca2e4c3c8d8672ba8fa18dd07
SoyAdV <79457088+SoyAdV@users.noreply.github.com>
2026-07-27 13:52:07 -0600

    Update Principal001.java

--- ProgramaEjemplo01/src/main/Principal001.java
+++ ProgramaEjemplo01/src/main/Principal001.java
@@ -5,9 +5,7 @@ public class Principal001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola1");
-		System.out.println("Hola2");
 		System.out.println("Hola3");
-		System.out.println("Hola4");
 		System.out.println("Hola5");
 
 	}
