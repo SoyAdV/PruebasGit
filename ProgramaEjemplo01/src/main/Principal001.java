46a6a89ef3632ba33c71c4a9c81812c99876a7dc
VICG059 <VICG059@VICG05908XLMXCO.telcel.net>
2026-07-27 11:03:57 -0600

    Se actualiza main

--- ProgramaEjemplo01/src/main/Principal001.java
+++ ProgramaEjemplo01/src/main/Principal001.java
@@ -5,6 +5,8 @@ public class Principal001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola");
+		System.out.print("Hola2");
+		System.out.print("Hola");
 
 	}
 
