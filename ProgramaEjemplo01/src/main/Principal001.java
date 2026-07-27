947a4199610c09acb027ab051b20a233378c7374
VICG059 <VICG059@VICG05908XLMXCO.telcel.net>
2026-07-27 14:08:09 -0600

    [master] AVF Se modifica main

--- ProgramaEjemplo01/src/main/Principal001.java
+++ ProgramaEjemplo01/src/main/Principal001.java
@@ -3,8 +3,10 @@ package main;
 public class Principal001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola1");
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("Hola " + (i+1));
+		}
 
 	}
 
