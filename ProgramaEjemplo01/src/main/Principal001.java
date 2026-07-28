9c66b74a5eb7de8a37a8ed881a4b7dc35f21a866
VICG059 <VICG059@VICG05908XLMXCO.telcel.net>
2026-07-28 09:30:51 -0600

    [master] AVF main modificado

--- ProgramaEjemplo01/src/main/Principal001.java
+++ ProgramaEjemplo01/src/main/Principal001.java
@@ -7,6 +7,15 @@ public class Principal001 {
 		for (int i=0; i<5; i++) {
 			System.out.println("Hola " + (i+1));
 		}
+		
+		System.out.println("");
+		
+		for (int i=0; i<8; i++) {
+			for (int j=0; j<i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 
 	}
 
